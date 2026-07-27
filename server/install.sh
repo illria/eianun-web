279d5a7f5a2e250153e759ae8350e270a47571b9
illria <qkwr521@gmail.com>
2026-07-27 16:43:28 +0800

    fix: bust updater script cache during install

--- server/install.sh
+++ server/install.sh
@@ -17,7 +17,7 @@ mkdir -p /usr/local/sbin
 
 UPDATE_TMP="$(mktemp /tmp/eianun-web-update.XXXXXX)"
 wget -qO "$UPDATE_TMP" \
-  https://raw.githubusercontent.com/illria/eianun-web/main/server/eianun-web-update.sh
+  "https://raw.githubusercontent.com/illria/eianun-web/main/server/eianun-web-update.sh?cache=$(date +%s)"
 tr -d '\r' < "$UPDATE_TMP" > /usr/local/sbin/eianun-web-update
 rm -f "$UPDATE_TMP"
 chmod 0755 /usr/local/sbin/eianun-web-update
