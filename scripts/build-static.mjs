38e564e6785abc3491c24027974b4829b68c8de2
illria <qkwr521@gmail.com>
2026-07-27 16:59:38 +0800

    fix: inline CSS in static HTML package

--- scripts/build-static.mjs
+++ scripts/build-static.mjs
@@ -1,10 +1,19 @@
-import { cp, mkdir, rm } from "node:fs/promises";
+import { cp, mkdir, readFile, rm, writeFile } from "node:fs/promises";
 
 await rm("dist-static", { recursive: true, force: true });
 await mkdir("dist-static", { recursive: true });
 await cp("static", "dist-static", { recursive: true });
 await rm("dist-static/icons", { recursive: true, force: true });
 await cp("public/site-data.json", "dist-static/site-data.json");
+
+const stylesheet = await readFile("static/site.css", "utf8");
+const inlineStyles = (html) => html.replace(
+  '  <link rel="stylesheet" href="./site.css">\n',
+  `  <style>\n${stylesheet}\n  </style>\n`
+);
+
+const rootHtml = await readFile("dist-static/index.html", "utf8");
+await writeFile("dist-static/index.html", inlineStyles(rootHtml));
 
 const routes = [
   "nifulei-bank",
@@ -19,7 +28,7 @@ const routes = [
 
 for (const route of routes) {
   await mkdir(`dist-static/${route}`, { recursive: true });
-  await cp("static/index.html", `dist-static/${route}/index.html`);
+  await writeFile(`dist-static/${route}/index.html`, inlineStyles(await readFile("static/index.html", "utf8")));
 }
 
 console.log(`Static site written to dist-static (${routes.length + 1} routes)`);
